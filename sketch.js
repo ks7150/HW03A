38408004afef97771f88870698951685a65cb7bb
ks7150 <144281932+ks7150@users.noreply.github.com>
2023-09-26 13:34:34 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,6 +1,496 @@
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  createCanvas(400, 600);
+
 }
 
-function draw() {
+function draw() {background(255,220,0)
+  fill ("black")
+  translate(50, 0);
+  ellipse(0, 20, 20);
+
+  translate(50, 20);
+  ellipse(0, 20, 20);
+
+  translate(50, -20);
+  ellipse(0, 20, 20);
+
+  translate(50, 20);
+  ellipse(0, 20, 20);
+
+  translate(50, -20);
+  ellipse(0, 20, 20);
+
+  translate(50, 20);
+  ellipse(0, 20, 20);
+
+  translate(50, -20);
+  ellipse(0, 20, 20);
+
+  translate(-300, 50);
+  ellipse(0, 20, 20);
+  translate(50, 20);
+  ellipse(0, 20, 20);
+
+  translate(50, -20);
+  ellipse(0, 20, 20);
+
+  translate(50, 20);
+  ellipse(0, 20, 20);
+
+  translate(50, -20);
+  ellipse(0, 20, 20);
+
+  translate(50, 20);
+  ellipse(0, 20, 20);
+
+  translate(50, -20);
+  ellipse(0, 20, 20);
+
+  translate(-300, 50);
+  ellipse(0, 20, 20);
+  translate(50, 20);
+  ellipse(0, 20, 20);
+
+  translate(50, -20);
+  ellipse(0, 20, 20);
+
+  translate(50, 20);
+  ellipse(0, 20, 20);
+
+  translate(50, -20);
+  ellipse(0, 20, 20);
+
+  translate(50, 20);
+  ellipse(0, 20, 20);
+
+  translate(50, -20);
+  ellipse(0, 20, 20);
+
+  translate(-300, 50);
+  ellipse(0, 20, 20);
+  translate(50, 20);
+  ellipse(0, 20, 20);
+
+  translate(50, -20);
+  ellipse(0, 20, 20);
+
+  translate(50, 20);
+  ellipse(0, 20, 20);
+
+  translate(50, -20);
+  ellipse(0, 20, 20);
+
+  translate(50, 20);
+  ellipse(0, 20, 20);
+
+  translate(50, -20);
+  ellipse(0, 20, 20);
+
+  translate(-300, 50);
+  ellipse(0, 20, 20);
+  translate(50, 20);
+  ellipse(0, 20, 20);
+
+  translate(50, -20);
+  ellipse(0, 20, 20);
+
+  translate(50, 20);
+  ellipse(0, 20, 20);
+
+  translate(50, -20);
+  ellipse(0, 20, 20);
+
+  translate(50, 20);
+  ellipse(0, 20, 20);
+
+  translate(50, -20);
+  ellipse(0, 20, 20);
+
+  translate(-300, 50);
+  ellipse(0, 20, 20);
+  translate(50, 20);
+  ellipse(0, 20, 20);
+
+  translate(50, -20);
+  ellipse(0, 20, 20);
+
+  translate(50, 20);
+  ellipse(0, 20, 20);
+
+  translate(50, -20);
+  ellipse(0, 20, 20);
+
+  translate(50, 20);
+  ellipse(0, 20, 20);
+
+  translate(50, -20);
+  ellipse(0, 20, 20);
+
+  translate(-300, 50);
+  ellipse(0, 20, 20);
+  translate(50, 20);
+  ellipse(0, 20, 20);
+
+  translate(50, -20);
+  ellipse(0, 20, 20);
+
+  translate(50, 20);
+  ellipse(0, 20, 20);
+
+  translate(50, -20);
+  ellipse(0, 20, 20);
+
+  translate(50, 20);
+  ellipse(0, 20, 20);
+
+  translate(50, -20);
+  ellipse(0, 20, 20);
+
+  translate(-300, 50);
+  ellipse(0, 20, 20);
+  translate(50, 20);
+  ellipse(0, 20, 20);
+
+  translate(50, -20);
+  ellipse(0, 20, 20);
+
+  translate(50, 20);
+  ellipse(0, 20, 20);
+
+  translate(50, -20);
+  ellipse(0, 20, 20);
+
+  translate(50, 20);
+  ellipse(0, 20, 20);
+
+  translate(50, -20);
+  ellipse(0, 20, 20);
+
+  translate(-300, 50);
+  ellipse(0, 20, 20);
+  translate(50, 20);
+  ellipse(0, 20, 20);
+
+  translate(50, -20);
+  ellipse(0, 20, 20);
+
+  translate(50, 20);
+  ellipse(0, 20, 20);
+
+  translate(50, -20);
+  ellipse(0, 20, 20);
+
+  translate(50, 20);
+  ellipse(0, 20, 20);
+
+  translate(50, -20);
+  ellipse(0, 20, 20);
+
+  translate(-300, 50);
+  ellipse(0, 20, 20);
+  translate(50, 20);
+  ellipse(0, 20, 20);
+
+  translate(50, -20);
+  ellipse(0, 20, 20);
+
+  translate(50, 20);
+  ellipse(0, 20, 20);
+
+  translate(50, -20);
+  ellipse(0, 20, 20);
+
+  translate(50, 20);
+  ellipse(0, 20, 20);
+
+  translate(50, -20);
+  ellipse(0, 20, 20);
+
+  translate(-300, 50);
+  ellipse(0, 20, 20);
+  translate(50, 20);
+  ellipse(0, 20, 20);
+
+  translate(50, -20);
+  ellipse(0, 20, 20);
+
+  translate(50, 20);
+  ellipse(0, 20, 20);
+
+  translate(50, -20);
+  ellipse(0, 20, 20);
+
+  translate(50, 20);
+  ellipse(0, 20, 20);
+
+  translate(50, -20);
+  ellipse(0, 20, 20);
+
+  translate(-300, 50);
+  ellipse(0, 20, 20);
+  translate(50, 20);
+  ellipse(0, 20, 20);
+
+  translate(50, -20);
+  ellipse(0, 20, 20);
+
+  translate(50, 20);
+  ellipse(0, 20, 20);
+
+  translate(50, -20);
+  ellipse(0, 20, 20);
+
+  translate(50, 20);
+  ellipse(0, 20, 20);
+
+  translate(50, -20);
+  ellipse(0, 20, 20);
+
+fill("black")
+translate (0,0,0)
+ellipse (0,-5,10)
+
+translate (-50,20,0)
+ellipse (0,-5,10)
+
+translate (-50,-20,0)
+ellipse (0,-5,10)
+
+translate (-50,20,0)
+ellipse (0,-5,10)
+
+translate (-50,-20,0)
+ellipse (0,-5,10)
+
+translate (-50,20,0)
+ellipse (0,-5,10)
+
+translate (-50,-20,0)
+ellipse (0,-5,10)
+ 
+
+translate(0, -50, [0])
+ellipse (0,-5,10)
+
+translate (50,20,0)
+ellipse (0,-5,10)
+
+translate (50,-20,0)
+ellipse (0,-5,10)
+
+translate (50,20,0)
+ellipse (0,-5,10)
+
+translate (50,-20,0)
+ellipse (0,-5,10)
+
+translate (50,20,0)
+ellipse (0,-5,10)
+
+translate (50,-20,0)
+ellipse (0,-5,10)
+
+translate (0,0,0)
+ellipse (0,-5,10)
+
+translate (-50,-30,0)
+ellipse (0,-5,10)
+
+translate (-50,-20,0)
+ellipse (0,-5,10)
+
+translate (-50,20,0)
+ellipse (0,-5,10)
+
+translate (-50,-20,0)
+ellipse (0,-5,10)
+
+translate (-50,20,0)
+ellipse (0,-5,10)
+
+translate (-50,-20,0)
+ellipse (0,-5,10)
+
+translate(300, 0, [0])
+ellipse (0,-5,10)
+
+translate (0,0,0)
+ellipse (0,-5,10)
+
+translate (-50,-30,0)
+ellipse (0,-5,10)
+
+translate (-50,-20,0)
+ellipse (0,-5,10)
+
+translate (-50,20,0)
+ellipse (0,-5,10)
+
+translate (-50,-20,0)
+ellipse (0,-5,10)
+
+translate (-50,20,0)
+ellipse (0,-5,10)
+
+translate (-50,-20,0)
+ellipse (0,-5,10)
+
+translate(300, 0, [0])
+ellipse (0,-5,10)
+translate (-50,-30,0)
+ellipse (0,-5,10)
+
+translate (-50,-20,0)
+ellipse (0,-5,10)
+
+translate (-50,20,0)
+ellipse (0,-5,10)
+
+translate (-50,-20,0)
+ellipse (0,-5,10)
+
+translate (-50,20,0)
+ellipse (0,-5,10)
+
+translate (-50,-20,0)
+ellipse (0,-5,10)
+
+translate(300, 0, [0])
+ellipse (0,-5,10)
+translate (-50,-30,0)
+ellipse (0,-5,10)
+
+translate (-50,-20,0)
+ellipse (0,-5,10)
+
+translate (-50,20,0)
+ellipse (0,-5,10)
+
+translate (-50,-20,0)
+ellipse (0,-5,10)
+
+translate (-50,20,0)
+ellipse (0,-5,10)
+
+translate (-50,-20,0)
+ellipse (0,-5,10)
+
+translate(300, 0, [0])
+ellipse (0,-5,10)
+translate (-50,-30,0)
+ellipse (0,-5,10)
+
+translate (-50,-20,0)
+ellipse (0,-5,10)
+
+translate (-50,20,0)
+ellipse (0,-5,10)
+
+translate (-50,-20,0)
+ellipse (0,-5,10)
+
+translate (-50,20,0)
+ellipse (0,-5,10)
+
+translate (-50,-20,0)
+ellipse (0,-5,10)
+
+translate(300, 0, [0])
+ellipse (0,-5,10)
+translate (-50,-30,0)
+ellipse (0,-5,10)
+
+translate (-50,-20,0)
+ellipse (0,-5,10)
+
+translate (-50,20,0)
+ellipse (0,-5,10)
+
+translate (-50,-20,0)
+ellipse (0,-5,10)
+
+translate (-50,20,0)
+ellipse (0,-5,10)
+
+translate (-50,-20,0)
+ellipse (0,-5,10)
+
+translate(300, 0, [0])
+ellipse (0,-5,10)
+translate (-50,-30,0)
+ellipse (0,-5,10)
+
+translate (-50,-20,0)
+ellipse (0,-5,10)
+
+translate (-50,20,0)
+ellipse (0,-5,10)
+
+translate (-50,-20,0)
+ellipse (0,-5,10)
+
+translate (-50,20,0)
+ellipse (0,-5,10)
+
+translate (-50,-20,0)
+ellipse (0,-5,10)
+
+translate(300, 0, [0])
+ellipse (0,-5,10)
+translate (-50,-30,0)
+ellipse (0,-5,10)
+
+translate (-50,-20,0)
+ellipse (0,-5,10)
+
+translate (-50,20,0)
+ellipse (0,-5,10)
+
+translate (-50,-20,0)
+ellipse (0,-5,10)
+
+translate (-50,20,0)
+ellipse (0,-5,10)
+
+translate (-50,-20,0)
+ellipse (0,-5,10)
+
+translate(300, 0, [0])
+ellipse (0,-5,10)
+translate (-50,-30,0)
+ellipse (0,-5,10)
+
+translate (-50,-20,0)
+ellipse (0,-5,10)
+
+translate (-50,20,0)
+ellipse (0,-5,10)
+
+translate (-50,-20,0)
+ellipse (0,-5,10)
+
+translate (-50,20,0)
+ellipse (0,-5,10)
+
+translate (-50,-20,0)
+ellipse (0,-5,10)
+
+translate(300, 0, [0])
+ellipse (0,-5,10)
+
+
+
+
+
+
+ 
+
+
+
+
+
+
+  
 }
+
+
